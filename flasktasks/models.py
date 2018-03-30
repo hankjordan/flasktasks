5acd642389ec9a8823425f9e44cabc5886c8b7aa
Hank Jordan <hank@henryjordan.com>
2018-03-30 12:54:06 -0400

    Converting tabs to spaces, fixing some 500 errors, future-proofing

--- flasktasks/models.py
+++ flasktasks/models.py
@@ -4,62 +4,62 @@ from time import strftime
 
 
 class Status(Enum):
-    TO_DO = 1
-    DOING = 2
-    DONE = 3
+	TO_DO = 1
+	DOING = 2
+	DONE = 3
 
 class Color(Enum):
-    GREY = 1
-    BLUE = 2
-    GREEN = 3
-    YELLOW = 4
-    RED = 5
+	GREY = 1
+	BLUE = 2
+	GREEN = 3
+	YELLOW = 4
+	RED = 5
 
 class Task(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(70))
-    description = db.Column(db.String(140))
-    status = db.Column(db.Integer)
-    mission_id = db.Column(db.Integer, db.ForeignKey('mission.id'))
+	id = db.Column(db.Integer, primary_key=True)
+	title = db.Column(db.String(70))
+	description = db.Column(db.String(140))
+	status = db.Column(db.Integer)
+	mission_id = db.Column(db.Integer, db.ForeignKey('mission.id'))
 
-    def __init__(self, title, description, mission_id):
-        self.title = title
-        self.description = description
-        self.status = Status.TO_DO.value
-        self.mission_id = mission_id
+	def __init__(self, title, description, mission_id):
+		self.title = title
+		self.description = description
+		self.status = Status.TO_DO.value
+		self.mission_id = mission_id
 
 class Mission(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(70), unique=True)
-    description = db.Column(db.String(210))
-    tag_id = db.Column(db.Integer, db.ForeignKey('tag.id'))
-    tasks = db.relationship('Task', backref='mission', lazy='dynamic')
+	id = db.Column(db.Integer, primary_key=True)
+	title = db.Column(db.String(70), unique=True)
+	description = db.Column(db.String(210))
+	tag_id = db.Column(db.Integer, db.ForeignKey('tag.id'))
+	tasks = db.relationship('Task', backref='mission', lazy='dynamic')
 
-    def __init__(self, title, description, tag_id):
-        self.title = title
-        self.description = description
-        self.tag_id = tag_id
+	def __init__(self, title, description, tag_id):
+		self.title = title
+		self.description = description
+		self.tag_id = tag_id
 
 class Tag(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20), unique=True)
-    color = db.Column(db.Integer)
-    missions = db.relationship('Mission', backref='tag', lazy='dynamic')
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(20), unique=True)
+	color = db.Column(db.Integer)
+	missions = db.relationship('Mission', backref='tag', lazy='dynamic')
 
-    def __init__(self, name, color=Color.GREY):
-        self.name = name
-        self.color = color.value
-    
-    def style(self):
-        color = Color(self.color)
-        return "tagged tag-%s" % color.name.lower()
+	def __init__(self, name, color=Color.GREY):
+		self.name = name
+		self.color = color.value
+	
+	def style(self):
+		color = Color(self.color)
+		return "tagged tag-%s" % color.name.lower()
 
 
 class LogEntry(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    timestamp = db.Column(db.String(30))
-    message = db.Column(db.String(140))
+	id = db.Column(db.Integer, primary_key=True)
+	timestamp = db.Column(db.String(30))
+	message = db.Column(db.String(140))
 
-    def __init__(self, message):
-        self.message = message
-        self.timestamp = strftime("%d-%m-%Y %H:%M:%S")
+	def __init__(self, message):
+		self.message = message
+		self.timestamp = strftime("%d-%m-%Y %H:%M:%S")
--- flasktasks/views.py
+++ flasktasks/views.py
@@ -7,105 +7,122 @@ from flasktasks.signals import task_created, mission_created
 
 @app.route('/')
 def index():
-    return render_template('home.html')
+	return render_template('home.html')
+	
+@app.route('/future')
+def future():
+	return render_template('future.html')
 
 @app.route('/missions')
 def missions():
-    missions = Mission.query.all()
-    return render_template('mission/index.html', missions=missions)
+	missions = Mission.query.all()
+	return render_template('mission/index.html', missions=missions)
 
 @app.route('/missions/new', methods=['POST', 'GET'])
 def new_mission():
-    if request.method == 'POST':
-        mission = Mission(request.form.get('title'),
-                          request.form.get('description'),
-                          request.form.get('tag_id'))
-        db.session.add(mission)
-        db.session.commit()
-        mission_created.send(mission)
-        return redirect(url_for('missions'))
-    else:
-        tags = Tag.query.all()
-        return render_template('mission/new.html', tags=tags)
+	if request.method == 'POST':
+		mission = Mission(request.form.get('title'),
+						  request.form.get('description'),
+						  request.form.get('tag_id'))
+		db.session.add(mission)
+		db.session.commit()
+		mission_created.send(mission)
+		return redirect(url_for('missions'))
+	else:
+		tags = Tag.query.all()
+		return render_template('mission/new.html', tags=tags)
 
 @app.route('/tasks')
 def tasks():
-    mission = None
-    if request.args.get('mission_id'):
-        mission = Mission.query.get_or_404(request.args.get('mission_id'))
-        tasks = Task.query.filter_by(mission_id=mission.id).all()
-    else:
-        tasks = Task.query.all()
+	mission = None
+	if request.args.get('mission_id'):
+		mission = Mission.query.get_or_404(request.args.get('mission_id'))
+		tasks = Task.query.filter_by(mission_id=mission.id).all()
+	else:
+		return redirect(url_for('missions'))
 
-    tasks_by_status = defaultdict(list)
-    for task in tasks:
-        status = Status(task.status).name 
-        tasks_by_status[status].append(task)
-    return render_template('task/index.html', tasks=tasks_by_status,
-                           mission=mission)
+	tasks_by_status = defaultdict(list)
+	for task in tasks:
+		status = Status(task.status).name 
+		tasks_by_status[status].append(task)
+	return render_template('task/index.html', tasks=tasks_by_status,
+						   mission=mission)
 
 @app.route('/tasks/new', methods=['POST', 'GET'])
 def new_task():
-    if request.method == 'POST':
-        task = Task(request.form.get('title'),
-                    request.form.get('description'),
-                    request.form.get('mission_id'))
-        db.session.add(task)
-        db.session.commit()
-        task_created.send(task)
-        return redirect(url_for('tasks'))
-    else:
-        missions = Mission.query.all()
-        return render_template('task/new.html', missions=missions)
+	if request.method == 'POST':
+		task = Task(request.form.get('title'),
+					request.form.get('description'),
+					request.form.get('mission_id'))
+		db.session.add(task)
+		db.session.commit()
+		task_created.send(task)
+		return redirect(url_for('tasks',mission_id=task.mission_id))
+	else:
+		missions = Mission.query.all()
+		return render_template('task/new.html', missions=missions)
 
 @app.route('/tasks/<int:task_id>')
 def task(task_id):
-    task = Task.query.get_or_404(task_id)
-    return render_template('task/task.html', task=task)
+	task = Task.query.get_or_404(task_id)
+	return render_template('task/task.html', task=task)
+	
+@app.route('/tasks/<int:task_id>/edit', methods=['POST','GET'])
+def edit_task(task_id):
+	if request.method == 'POST':
+		task = Task.query.get_or_404(task_id)
+		task.title = request.form.get('title')
+		task.description = request.form.get('description')
+		db.session.commit()
+		return redirect(url_for('tasks',mission_id=task.mission_id))
+	else:
+		task = Task.query.get_or_404(task_id)
+		return render_template('task/edit.html', task=task)
 
 @app.route('/tasks/<int:task_id>/set_status/<status>')
 def set_status(task_id, status):
-    task = Task.query.get_or_404(task_id)
-    try:
-        task.status = Status[status.upper()].value
-    except KeyError:
-        abort(400)
+	task = Task.query.get_or_404(task_id)
+	try:
+		task.status = Status[status.upper()].value
+	except KeyError:
+		abort(400)
 
-    db.session.add(task)
-    db.session.commit()
-    return redirect(url_for('tasks'))
-
+	db.session.add(task)
+	db.session.commit()
+	return redirect(url_for('tasks'))
+	
 @app.route('/tasks/<int:task_id>', methods=['DELETE'])
 def delete_task(task_id):
-    task = Task.query.get_or_404(task_id)
-    db.session.delete(task)
-    db.session.commit()
-    return url_for('tasks')
-    
+	task = Task.query.get_or_404(task_id)
+	mission = task.mission_id
+	db.session.delete(task)
+	db.session.commit()
+	return url_for('tasks',mission_id=mission)
+	
 @app.route('/missions/<int:mission_id>', methods=['DELETE'])
 def delete_mission(mission_id):
-    mission = Mission.query.get_or_404(mission_id)
-    db.session.delete(mission)
-    db.session.commit()
-    return url_for('missions')
+	mission = Mission.query.get_or_404(mission_id)
+	db.session.delete(mission)
+	db.session.commit()
+	return url_for('missions')
 
 @app.route('/tags/new', methods=['POST', 'GET'])
 def new_tag():
-    if request.method == 'POST':
-        try:
-            color = Color(int(request.form.get('color_id')))
-        except ValueError:
-            abort(400)
-        tag = Tag(request.form.get('name'), color)
-        db.session.add(tag)
-        db.session.commit()
-        return redirect(url_for('missions'))
-    else:
-        colors = { color.name: color.value for color in Color }
-        return render_template('tags/new.html', colors=colors)
+	if request.method == 'POST':
+		try:
+			color = Color(int(request.form.get('color_id')))
+		except ValueError:
+			abort(400)
+		tag = Tag(request.form.get('name'), color)
+		db.session.add(tag)
+		db.session.commit()
+		return redirect(url_for('missions'))
+	else:
+		colors = { color.name: color.value for color in Color }
+		return render_template('tags/new.html', colors=colors)
 
 
 @app.route('/log')
 def log():
-    log_entries = LogEntry.query.all()
-    return render_template('log.html', log_entries=log_entries)
+	log_entries = LogEntry.query.all()
+	return render_template('log.html', log_entries=log_entries)
